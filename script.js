e5db7cb4c789da6d014b566e6ae386466af8da26
Albert Zhan <albert.cfzhan@gmail.com>
2022-02-23 16:01:37 -0800

    created grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+for (let i=0; i<16; i++) {
+    for (let j=0; j<16; j++) {
+        const div = document.createElement('div');
+        let divStyle = div.style;
+        container.appendChild(div);
+        divStyle.borderWidth = '5px';
+        divStyle.borderStyle = 'solid';
+        divStyle.width = '50px';
+        divStyle.height = '50px';
+    }
+}
