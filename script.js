26ba317f3832dd099d27fb4b54b5de7887652499
Albert Zhan <albert.cfzhan@gmail.com>
2022-02-23 17:14:47 -0800

    set limit for dimensions to prevent crashing created button that changes border width (with max)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let borderWidth = 4;
+let dimensions = 16;
+let skipDimensionsPrompt = true;
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -6,8 +8,17 @@ function removeAllChildNodes(parent) {
     }
 }
 
+//FIXME: when button is canceled, it still deletes old grid
+// FIXME: if you just click ok without input, it becomes wack
+// also the buttons go up when they shouldn't move, grid should move down if absolutely necessary
 function newGrid() {
-    const dimensions = prompt('enter dimensions of new grid');
+    if (!skipDimensionsPrompt) {
+        dimensions = prompt('enter dimensions of new grid (max 100)');
+        while (dimensions > 100) {
+            dimensions = prompt('enter dimensions of new grid (max 100)');
+        }
+    }
+    skipDimensionsPrompt = false;
     removeAllChildNodes(container);
     for (let i=0; i<dimensions; i++) {
         for (let j=0; j<dimensions; j++) {
@@ -25,20 +36,18 @@ function newGrid() {
     }
 }
 
-
-const container = document.querySelector('.container');
-for (let i=0; i<16; i++) {
-    for (let j=0; j<16; j++) {
-        const div = document.createElement('div');
-        let divStyle = div.style;
-        container.appendChild(div);
-        divStyle.borderWidth = `${borderWidth}px`;
-        divStyle.borderStyle = 'solid';
-        divStyle.width = `${960 / 16 - 2 * borderWidth}px`;
-        divStyle.height = `${960 / 16 - 2 * borderWidth}px`;
-        div.addEventListener('mouseover', () => div.classList.add('hovered'));
-    }
+function changeBorderWidth() {
+    let newBorderWidth = prompt('enter new border width (reccomended 4 or less)');
+    borderWidth = newBorderWidth;
+    skipDimensionsPrompt = true;
+    newGrid();
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', newGrid);
+
+const container = document.querySelector('.container');
+newGrid();
+
+const clearButton = document.querySelector('.clearGrid');
+clearButton.addEventListener('click', newGrid);
+const outlineButton = document.querySelector('.borderWidth');
+outlineButton.addEventListener('click', changeBorderWidth);
