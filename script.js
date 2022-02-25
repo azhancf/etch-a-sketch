b9217302ebbd7d3a70c7e6d72205ef6d19aad723
Albert Zhan <albert.cfzhan@gmail.com>
2022-02-24 19:11:43 -0800

    fixed draggable div bug organized code a bit added color mode button that allows you to switch between filling with black and filling with random rgb colors

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ let borderWidth = 1;
 let dimensions = 16;
 let skipDimensionsPrompt = true;
 let isMouseDown = false;
+let colorMode = 1;
 
+
+// utility functions
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 /*
@@ -14,8 +21,9 @@ FIXME: when button is canceled, it still deletes old grid
 FIXME: if you just click ok without input, it becomes wack
 also the buttons go up when they shouldn't move, grid should move down if absolutely necessary
 TODO: eraser tool or just when on dark square
-FIXME: dragging doesn't always work 
 */
+
+// BUTTONS 
 function newGrid() {
     if (!skipDimensionsPrompt) {
         dimensions = prompt('enter dimensions of new grid (max 100)');
@@ -34,19 +42,50 @@ function newGrid() {
             divStyle.borderStyle = 'solid';
             divStyle.width = `${960 / dimensions - 2 * borderWidth}px`;
             divStyle.height = `${960 / dimensions - 2 * borderWidth}px`;
-            div.addEventListener('mouseover', () => {
-                if (isMouseDown) div.classList.add('hovered')});
+            div.addEventListener('mouseover', () => changeSquareColor(div));
         }
     }
 }
 
 function changeBorderWidth() {
-    let newBorderWidth = prompt('enter new border width (reccomended 4 or less)');
+    let newBorderWidth = prompt('enter new border width (recommended 4 or less)');
     borderWidth = newBorderWidth;
     skipDimensionsPrompt = true;
     newGrid();
 }
 
+function changeColorMode() {
+    if (colorMode === 2) { // amount of color types
+        colorMode = 1;
+    }
+    else {
+        colorMode++;
+    }
+}
+
+// functions that change square colors
+function changeSquareColor(node) {
+    if (isMouseDown) {
+        switch (colorMode) {
+            case 1:
+                fillBlack(node);
+                break;
+            case 2:
+                fillRandomColor(node);
+                break;
+        }
+    }
+}
+
+function fillBlack(node) {
+    node.style.backgroundColor = 'black';
+}
+
+function fillRandomColor(node) {
+    node.style.backgroundColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
+}
+
+// actual content/creation
 document.addEventListener('mousedown', function(event) { 
     if (event) isMouseDown = true;
 }, true);
@@ -62,3 +101,5 @@ const clearButton = document.querySelector('.clearGrid');
 clearButton.addEventListener('click', newGrid);
 const outlineButton = document.querySelector('.borderWidth');
 outlineButton.addEventListener('click', changeBorderWidth);
+const modeButton = document.querySelector('.colorMode');
+modeButton.addEventListener('click', changeColorMode);
